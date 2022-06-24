1d013d466f1a3149b3ff5341666cc9c49028eb44
mbebic <mjb@achillearesearch.com>
2022-06-24 09:30:07 -0400

    Update urls.py

--- curveproj/curveproj/urls.py
+++ curveproj/curveproj/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('curveapp.urls')),
-    path('', include('calcconic.urls')),
+    path('calcconic/', include('calcconic.urls')),
     path('admin/', admin.site.urls),
 ]
