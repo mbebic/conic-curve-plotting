f917153dabae930e180babfb460f6984d22fb107
mbebic <mjb@achillearesearch.com>
2022-06-23 22:21:17 -0400

    shows plots, fixed server mistake etc

--- curveproj/curveapp/views.py
+++ curveproj/curveapp/views.py
@@ -33,26 +33,6 @@ def index(request):
         "form": form
     })
 
-def conicgraph(request):
-    x = [-2,0,4,6,7]
-    y = [q**2-q+3 for q in x]
-    x1 = [-3,1,5,7,9]
-    y1 = [2*q**2-q-1 for q in x1]
-    trace = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': 10},
-                        mode="lines",  name='1st Trace')
-
-    trace1 = go.Scatter(x=x1, y=y1, marker={'color': 'blue', 'symbol': 104, 'size': 10},
-                        mode="lines",  name='2nd Trace')
-
-    layout=go.Layout(title="Meine Daten", xaxis={'title':'x'}, yaxis={'title':'y'})
-    figure=go.Figure(data=[trace, trace1],layout=layout)
-
-    return render(request, 'curveapp/conicgraph.html', {
-        "graph": figure.to_html()
-    })
-
-# def testlookup(request, curve_id):
-#     curve = conicselection.objects.get(pk=curve_id)
 
 @ensure_csrf_cookie
 def library(request):
