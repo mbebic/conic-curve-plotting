4d8156f15c6a01773c5c8bac220299c5770fe092
mbebic <mjb@achillearesearch.com>
2022-06-24 23:41:06 -0400

    added permissions and user authentication

--- curveproj/curveapp/views.py
+++ curveproj/curveapp/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
@@ -12,6 +12,9 @@ from .forms import conicForm
 from .models import conicselection
 
 # Create your views here.
+
+def is_member(user):
+    return user.groups.filter(name="CircleGroup").exists()
 
 @csrf_protect
 @ensure_csrf_cookie
@@ -33,10 +36,48 @@ def index(request):
         "form": form
     })
 
-
+# @login_required(login_url="login")
+# @user_passes_test(is_member, login_url="error")
 @ensure_csrf_cookie
 def library(request):
     return render(request, "curveapp/library.html", {
-        "curvedata": conicselection.objects.all(),
-    })
+            "curvedata": conicselection.objects.all(),
+            "permissions": user.get_group_permissions()
+        })
+    # if is_member(request.user, "CircleGroup"):
+    #     return render(request, "curveapp/library.html", {
+    #         "curvedata": conicselection.objects.all(),
+    #     })
+    # else:
+    #     return render(request, "curveapp/error.html", {
+    #         "message": "You are not authorized to view this page. Contact your supervisor to allow permissions."
+    #     }) 
 
+def error_page(request):
+    return render(request, "curveapp/error.html", {
+            "message": "You are not authorized to view this page. Contact your supervisor to allow permissions."
+        }) 
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            # return render(request, "curveapp/index.html")
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "curveapp/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "curveapp/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
